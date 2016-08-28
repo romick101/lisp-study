65c1c338b18c74bc31f4b2c87ad654815ecfeb86
romick101 <romick101@gmail.com>
2016-08-28 12:47:26 +0300

    update gitignore

--- PL_CFG_DFA_STYLYK/main.lisp
+++ PL_CFG_DFA_STYLYK/main.lisp
@@ -1,12 +1,13 @@
 (defun load-files ()
-  (load "/home/romick101/lisp/PL_CFG_DFA/LP.lisp")
-  (load "/home/romick101/lisp/PL_CFG_DFA/CFG.lisp")
-  (load "/home/romick101/lisp/PL_CFG_DFA/DFA.lisp")
-  (load "/home/romick101/lisp/PL_CFG_DFA/CT4_Stylyk.lisp"))
+  (load "/home/romick101/lisp/PL_CFG_DFA_STYLYK/LP.lisp")
+  (load "/home/romick101/lisp/PL_CFG_DFA_STYLYK/CFG.lisp")
+  (load "/home/romick101/lisp/PL_CFG_DFA_STYLYK/DFA.lisp")
+  (load "/home/romick101/lisp/PL_CFG_DFA_STYLYK/CT4_Stylyk.lisp"))
 
 (defun main ()
   (load-files)
-  (test-building "/home/romick101/lisp/PL_CFG_DFA/res/pure_CFG.dot")
-  (test-liveness "/home/romick101/lisp/PL_CFG_DFA/res/CFG_LIVENESS.dot") ;; not correct
-  (test-RD "/home/romick101/lisp/PL_CFG_DFA/res/CFG_RD.dot")
-  (test-altering "/home/romick101/lisp/PL_CFG_DFA/res/before-prop.dot" "/home/romick101/lisp/PL_CFG_DFA/res/after-prop.dot"))
+  (test-building "/home/romick101/lisp/PL_CFG_DFA_STYLYK/res/pure_CFG.dot")
+  (test-liveness "/home/romick101/lisp/PL_CFG_DFA_STYLYK/res/CFG_LIVENESS.dot") ;; not correct
+  (test-RD "/home/romick101/lisp/PL_CFG_DFA_STYLYK/res/CFG_RD.dot")
+  (test-altering "/home/romick101/lisp/PL_CFG_DFA_STYLYK/res/before-prop.dot"
+		 "/home/romick101/lisp/PL_CFG_DFA_STYLYK/res/after-prop.dot"))
